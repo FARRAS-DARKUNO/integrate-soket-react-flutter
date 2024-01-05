b1573e9a645b742efb7b0b79fe9ce26f7a8bc34c
FARRAS-DARKUNO <83520627+FARRAS-DARKUNO@users.noreply.github.com>
2024-01-05 15:06:57 +0700

    change backround

---
+++ website/src/components/textInput/inpputSand/inputSand.tsx
@@ -0,0 +1,8 @@
+
+const InputSand = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default InputSand
---
+++ website/src/page/chatPage/chatPage.tsx
@@ -0,0 +1,13 @@
+import InputSand from '../../components/textInput/inpputSand/inputSand'
+import './chatPage.css'
+const ChatPage = () => {
+    return (
+        <div className="board">
+            <div>
+                <InputSand/>
+            </div>
+        </div>
+    )
+}
+
+export default ChatPage
